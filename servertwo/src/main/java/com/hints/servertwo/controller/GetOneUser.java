b7574dc5f9a315c7c7e53a3b664b79e0cf2cd6ae
180686 <180686@gree.com>
2020-09-24 10:42:08 +0800

    这是我的第二次上传

--- servertwo/src/main/java/com/hints/servertwo/controller/GetOneUser.java
+++ servertwo/src/main/java/com/hints/servertwo/controller/GetOneUser.java
@@ -27,7 +27,7 @@ public class GetOneUser {
 
     @GetMapping("/user/{id}")
     public String getOneUserById(@PathVariable String id) {
-        System.out.println("get");
+        System.out.println("get2");
         return cronTaskDao.getTest()+port;
     }
 

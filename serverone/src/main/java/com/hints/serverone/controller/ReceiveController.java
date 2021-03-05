ff0b9e88f68681e5d3f04a65ec973e5b6bae8d2e
hints0816 <44020055+hints0816@users.noreply.github.com>
2021-03-05 11:38:59 +0800

    Create ReceiveController.java

--- serverone/src/main/java/com/hints/serverone/controller/ReceiveController.java
+++ serverone/src/main/java/com/hints/serverone/controller/ReceiveController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReceiveController {
     @StreamListener("message")
     public void onReceive(String messageInfo) {
-        System.out.println("接受到的消息2：" + messageInfo);
+        System.out.println("接受到的消息3：" + messageInfo);
     }
 }
